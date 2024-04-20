1b6958a9368e73ac8df4c30471ec2b0e67684bca
Maria <mbeaatriz@gmail.com>
2024-04-19 21:31:54 -0300

    fix: ajustando componente ProductCardCategory

--- src/src/Routes.js
+++ src/src/Routes.js
@@ -19,7 +19,7 @@ export default function Routes() {
   return (
     <NavigationContainer theme={Theme}>
       <Navigator
-        initialRouteName="Home"
+        initialRouteName="MyPlants"
         screenOptions={{
           headerShown: false,
         }}>
--- src/src/pages/Home.js
+++ src/src/pages/Home.js
@@ -94,7 +94,6 @@ export default function Home() {
           </View>
         </View>
       </ScrollView>
-
       
       <NavigationBarBottom/>
     </>
