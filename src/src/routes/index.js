24329d9ed4cb13a44dff109c4a5fd4170330af31
Maria <mbeaatriz@gmail.com>
2024-05-18 18:14:53 -0300

    corrigindo detalhes

--- src/src/routes/index.js
+++ src/src/routes/index.js
@@ -19,7 +19,7 @@ export default function Routes() {
   return (
     <NavigationContainer theme={Theme}>
       <Navigator
-        initialRouteName="Settings"
+        initialRouteName="MyPlants"
         screenOptions={{
           headerShown: false,
         }}
