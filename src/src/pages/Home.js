782cdf2e42fd54044987fa617c44f401177a71d8
Lucas Lousada <lucas_lousada@outlook.com>
2024-05-26 09:27:17 -0300

    fix: o botão que redireciona para os artigos não estava funcionando

--- src/src/pages/Home.js
+++ src/src/pages/Home.js
@@ -85,7 +85,7 @@ export default function Home() {
                   </TouchableOpacity>
                 </View>
                 <View style={styles.navItem}>
-                  <TouchableOpacity onPress={() => console.log("Pressed")}>
+                  <TouchableOpacity onPress={() => navigate("ArticlesHome")}>
                     <Article width={24} height={24} marginBottom={10} />
                     <Text style={styles.textColor} variant="bodySmall">
                       Guia{"\n"}verde
